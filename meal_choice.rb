1d3425a8b852bc2c1d41d9295833627300813094
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-12 11:43:06 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,8 @@
+request pry
+
+def meal_choice(meal='meat')
+ binding.pry  
+ puts meal
+end
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
